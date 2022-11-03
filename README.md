39c3d67c0c60a64dd085981ad11cc2afc4e3535e
Ken Arroyo Ohori <k.ohori@tudelft.nl>
2022-11-03 23:26:48 +0100

    code, license and readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # osmium-buildings
-Simple script to count OSM buildings using the Osmium library
+
+Simple script to count OSM buildings using the Osmium library.
+
+Links: [map](https://3d.bk.tudelft.nl/ken/maps/osm-buildings/) and [data](https://3d.bk.tudelft.nl/ken/maps/osm-buildings/buildings.csv)
---
+++ main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <fstream>
+
+#include <osmium/area/assembler.hpp>
+#include <osmium/area/multipolygon_manager.hpp>
+#include <osmium/handler/node_locations_for_ways.hpp>
+#include <osmium/io/pbf_input.hpp>
+#include <osmium/visitor.hpp>
+#include <osmium/index/map/flex_mem.hpp>
+
+using index_type = osmium::index::map::FlexMem<osmium::unsigned_object_id_type, osmium::Location>;
+using location_handler_type = osmium::handler::NodeLocationsForWays<index_type>;
+
+class BuildingHandler : public osmium::handler::Handler {
+public:
+  const int degree_divisions = 10;  // how many divisions per degree (lat and long)
+  std::size_t buildings;
+  std::vector<std::vector<std::size_t>> buildings_total;
+  
+  BuildingHandler() {
+    buildings = 0;
+    for (int lon = 0; lon < 360*degree_divisions; ++lon) {
+      buildings_total.emplace_back(std::vector<std::size_t>());
+      for (int lat = 0; lat < 180*degree_divisions; ++lat) {
+        buildings_total.back().push_back(0);
+      }
+    }
+  }
+  
+  void area(const osmium::Area& area) {
+    double sum_lon = 0, sum_lat = 0;
+    std::size_t num_points = 0;
+    for (auto const &ring: area.outer_rings()) {
+      for (auto const &node: ring) {
+//        if (buildings < 100) std::cout << "Point at " << node.lon() << ", " << node.lat() << std::endl;
+        sum_lon += node.lon();
+        sum_lat += node.lat();
+        ++num_points;
+      }
+    } if (num_points > 0) {
+//      if (buildings < 100) std::cout << "Centroid at " << sum_lon/num_points << ", " << sum_lat/num_points << std::endl;
+      long longitude = std::round(degree_divisions*sum_lon/num_points);
+      long latitude = std::round(degree_divisions*sum_lat/num_points);
+      if (buildings % 1000000 == 0) std::cout << "\t" << buildings/1000000 << "m buildings processed " << std::endl;
+      ++buildings;
+      ++buildings_total[(longitude+180*degree_divisions)%(360*degree_divisions)][(latitude+90*degree_divisions)%(180*degree_divisions)];
+    }
+  }
+};
+
+int main(int argc, const char * argv[]) {
+  
+  BuildingHandler handler;
+  
+  std::vector<std::string> input_paths;
+  const std::filesystem::path data_folder{"/Users/ken/Downloads/osm/"}; // folder with OSM .pbf files
+  for (auto const& input_path : std::filesystem::directory_iterator{data_folder}) {
+    if (input_path.path().extension() == ".pbf") {
+      input_paths.push_back(input_path.path().string());
+    } else {
+      std::cout << "Skipping " << input_path.path().string() << "..." << std::endl;
+    }
+  }
+
+  for (auto const &input_path: input_paths) {
+    std::cout << "Reading input: " << input_path << "..." << std::endl;
+    
+    osmium::io::File input_file{input_path};
+    osmium::area::Assembler::config_type assembler_config;
+    
+    osmium::TagsFilter filter{false};
+    filter.add_rule(true, "building");
+    
+    osmium::area::MultipolygonManager<osmium::area::Assembler> mp_manager{assembler_config, filter};
+    
+    // First pass
+    std::cout << "\tReading relations...";
+    osmium::relations::read_relations(input_file, mp_manager);
+    std::cout << " done" << std::endl;
+    
+//    std::cerr << "Memory:\n";
+//    osmium::relations::print_used_memory(std::cerr, mp_manager.used_memory());
+    
+    index_type index;
+    location_handler_type location_handler{index};
+    location_handler.ignore_errors();
+    
+    std::cout << "\tReading objects..." << std::endl;
+    osmium::io::Reader reader{input_file};
+    osmium::apply(reader, location_handler, mp_manager.handler([&handler](osmium::memory::Buffer&& buffer) {
+      osmium::apply(buffer, handler);
+    }));
+    reader.close();
+    
+//    std::cerr << "Memory:\n";
+//    osmium::relations::print_used_memory(std::cerr, mp_manager.used_memory());
+  }
+  
+  std::ofstream output_stream;
+  output_stream.open("/Users/ken/Versioned/my-website/maps/osm-buildings/buildings.csv"); // output csv file
+  output_stream << "lat,lng,buildings\n";
+  for (int i = 0; i < 360*handler.degree_divisions; ++i) {
+    for (int j = 0; j < 180*handler.degree_divisions; ++j) {
+      if (handler.buildings_total[i][j] > 0) {
+        output_stream << (j/double(handler.degree_divisions))-90.0 << "," << (i/double(handler.degree_divisions))-180.0 << "," << handler.buildings_total[i][j] << "\n";
+      }
+    }
+  } output_stream.close();
+  
+  return 0;
+}
